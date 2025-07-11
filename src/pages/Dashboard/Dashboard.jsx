aefa190d83daf44318aa85f357cdcab86d3be3bd
yasserelshabrawy <yasser.elshabrawy98@gmail.com>
2025-07-11 22:15:28 +0300

    create table agents

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import Systemflow from './Systemflow'
-import Chart from './Chart'
-import TopAgencyTable from './TopAgencyTable'
-import { getStats } from '../../services/statsService';
-import LoadingSpinner from '../../components/ui/LoadingSpinner';
-import { getChartData } from '../../services/chartService';
+import React, { useEffect, useState } from "react";
+import Systemflow from "./Systemflow";
+import Chart from "./Chart";
+import TopAgencyTable from "./TopAgencyTable";
+import { getStats } from "../../services/statsService";
+import LoadingSpinner from "../../components/ui/LoadingSpinner";
+import { getChartData } from "../../services/chartService";
+import { getTopAgents } from "../../services/topAgents";
 
 export default function Dashboard() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [stats, setStats] = useState([]);
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [dataTable, setDataTable] = useState(null);
 
   useEffect(() => {
-    Promise.all([getStats(), getChartData()])
-      .then(([statsData , chartData]) => {
+    Promise.all([getStats(), getChartData(),getTopAgents()])
+      .then(([statsData, chartData,tabelData]) => {
         setStats(statsData);
         setData(chartData);
+        setDataTable(tabelData);
       })
       .catch((err) => console.error(err))
       .finally(() => setLoading(false));
   }, []);
-    if (loading) return <LoadingSpinner /> ;
+  if (loading) return <LoadingSpinner />;
   return (
-        <div className="space-y-6 bg-gray-50 p-6 container">
-          <div className=''>
-
-      <Systemflow stats={stats} />
-      <div className='grid md:grid md:grid-cols-2 items-center my-4'>
-      <Chart data={data} />
-      <TopAgencyTable  />
+    <div className="space-y-6 bg-gray-50 p-3 container">
+      <div className="">
+        <Systemflow stats={stats} />
+        <div className="grid grid-cols-1 md:grid-cols-2 items-start my-4 gap-5">
+          <Chart data={data} />
+          <TopAgencyTable dataTable={dataTable} />
+        </div>
       </div>
-          </div>
     </div>
-  )
+  );
 }
--- src/pages/Dashboard/TopAgencyTable.jsx
+++ src/pages/Dashboard/TopAgencyTable.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import agentNameBodyTemplate from "../../components/templates/agentNameBodyTemplate";
 
-export default function TopAgencyTable() {
+export default function TopAgencyTable({ dataTable }) {
   return (
-    <div>
-      
-        <h1 className="text-2xl font-bold">Top Agency Table</h1>
+    <div className="bg-white p-4 rounded shadow w-full max-w-4xl mx-auto">
+      <div className="flex items-center mb-4">
+        <div className="flex items-center justify-center p-2 w-8 h-8 bg-blue-100 rounded-md me-2">
+          <svg
+            className="w-6 h-6 text-blue-400"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11,3V23H5V3A2.006,2.006,0,0,1,7,1H9A2.006,2.006,0,0,1,11,3Z"
+              fill="currentColor"
+            />
+            <path
+              d="M21,11V23H15V11a2.006,2.006,0,0,1,2-2h2A2.006,2.006,0,0,1,21,11Z"
+              fill="currentColor"
+            />
+            <path
+              d="M23,24H4a4,4,0,0,1-4-4V1A1,1,0,0,1,2,1V20a2,2,0,0,0,2,2H23a1,1,0,0,1,0,2Z"
+              fill="currentColor"
+            />
+            <path
+              d="M23,22H22V11a3,3,0,0,0-3-3H17a3,3,0,0,0-3,3V22H12V3A3,3,0,0,0,9,0H7A3,3,0,0,0,4,3V22a2,2,0,0,1-2-2V1A1,1,0,0,0,0,1V20a4,4,0,0,0,4,4H23a1,1,0,0,0,0-2ZM6,22V3A1,1,0,0,1,7,2H9a1,1,0,0,1,1,1V22Zm10,0V11a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V22Z"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
+        <h2 className="text-lg font-bold ">Top 10 agents panel </h2>
+      </div>
+      <div className="card">
+        <DataTable
+          scrollable
+          scrollHeight="300px"
+          value={dataTable}
+          rows={10}
+          className="text-sm border-separate border-spacing-0"
+          rowClassName={() => "border-b border-gray-200"}
+        >
+          <Column
+            field="rank"
+            header="Rank"
+            headerClassName="bg-blue-100 p-2"
+            bodyClassName="p-2"
+          />
+          <Column
+            field="agentName"
+            header="Agent name"
+            headerClassName="bg-blue-100 p-2"
+            bodyClassName="p-2"
+            body={agentNameBodyTemplate}
+          />
+          <Column
+            field="callsRouted"
+            header="Calls routed"
+            headerClassName="bg-blue-100 p-2"
+            bodyClassName="p-2"
+          />
+          <Column
+            field="dropData"
+            header="Drop data"
+            headerClassName="bg-blue-100 p-2"
+            bodyClassName="p-2"
+          />
+          <Column
+            field="insuranceSuccess"
+            header="Insurance success"
+            headerClassName="bg-blue-100 p-2"
+            bodyClassName="p-2"
+          />
+        </DataTable>
+      </div>
     </div>
-  )
+  );
 }
